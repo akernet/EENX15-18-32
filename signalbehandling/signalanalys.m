c826d0682bbd3741c346e6aa9b6ca468e311f2c3
Jens <jenszza@gmail.com>
2018-04-23 11:01:00 +0200

    New matlab script for analysis

--- signalbehandling/signalanalys.m
+++ signalbehandling/signalanalys.m
@@ -2,94 +2,85 @@
 clear;
 clc;
 clf;
-samp_rate = 6e5; % samp/second
-fft_size = 2^10; % FFT-bin size
-windowSize = 4096;
-b = (1/windowSize)*ones(1,windowSize);
-a = 1;
+addpath('../../signalbehandling/figures');
+sampRate = 6e5; % samp/second
+fftSize = 2^10; % FFT-bin size
 
 % Read the first data file. Assign to complex is done later
 % after the the reference file is read because of reasons
-in_file_handler = fopen('outfile.bin');
-out1 = fread(in_file_handler, 'float');
-fclose(in_file_handler);
+inFileHandler = fopen('outfile.bin');
+out1 = fread(inFileHandler, 'float');
+fclose(inFileHandler);
 
 % Read the second data file and assign complex
-in_file_handler = fopen('outfile2.bin');
-out2 = fread(in_file_handler, 'float');
+inFileHandler = fopen('outfile2.bin');
+out2 = fread(inFileHandler, 'float');
 length(out2)/2
-re_out2 = out2(1:2:end);
-im_out2 = out2(2:2:end);
-out2 = re_out2 + 1i * im_out2;
-fclose(in_file_handler);
+reOut2 = out2(1:2:end);
+imOut2 = out2(2:2:end);
+out2 = reOut2 + 1i * imOut2;
+fclose(inFileHandler);
 
 % Read the reference file and make it as long as the output data files
 % Also assign out1 to complex
-in_file_handler = fopen('infile.bin');
-in = fread(in_file_handler, 'float');
+inFileHandler = fopen('infile.bin');
+in = fread(inFileHandler, 'float');
 length(in)/2
 in = repmat(in, ceil(length(out1)/length(in)), 1);
 in = in(1:length(out1));
-re_in = in(1:2:end);
-im_in = in(2:2:end);
-in = re_in + 1i * im_in;
-fclose(in_file_handler);
-re_out1 = out1(1:2:end);
-im_out1 = out1(2:2:end);
-out1 = re_out1 + 1i * im_out1;
+reIn = in(1:2:end);
+imIn = in(2:2:end);
+in = reIn + 1i * imIn;
+fclose(inFileHandler);
+reOut1 = out1(1:2:end);
+imOut1 = out1(2:2:end);
+out1 = reOut1 + 1i * imOut1;
 
-% Create matrix used to partition the data into fft_size:d bins
-repetitions = floor(length(out1)/fft_size)-1;
-bin_matrix = repmat((1:fft_size) , repetitions, 1) + transpose(fft_size*(1:repetitions));
+% Create matrix used to partition the data into fftSize:d bins
+repetitions = floor(length(out1)/fftSize)-1;
+binMatrix = repmat((1:fftSize) , repetitions, 1) + transpose(fftSize*(0:repetitions-1));
 
 % Partition the data into bins
-out1_bin = out1(bin_matrix);
-out2_bin = out2(bin_matrix);
-in_bin = in(bin_matrix);
+out1Bin = out1(binMatrix);
+out2Bin = out2(binMatrix);
+inBin = in(binMatrix);
 
 % Get the frequency domain of the data with the desired number of
 % samples/bin
-out1_bin_fft = fft(out1_bin, [], 2);
-out2_bin_fft = fft(out2_bin, [], 2);
-in_bin_fft = fft(in_bin, [], 2);
+out1BinFft = fft(out1Bin, [], 2);
+out2BinFft = fft(out2Bin, [], 2);
+inBinFft = fft(inBin, [], 2);
 
-% Plot the argument difference between output data and reference data
-subplot(1,2,1)
-plot(1:repetitions, unwrap(angle(out1_bin_fft(:,81)) - angle(in_bin_fft(:,81))))
+% Generate phase difference plot
+figure('rend','painters','pos',[10 10 700 300])
+plot((1:repetitions)*fftSize/sampRate, (angle(out2BinFft(:,81)./inBinFft(:,81))),'blue',...
+    (1:repetitions)*fftSize/sampRate, (angle(out1BinFft(:,81)./inBinFft(:,81))),'black--','LineWidth',1)
+set(gca, 'FontSize', 15)
+xlabel('Tid (s)','FontSize',15)
+ylabel('Fasskillnad (rad)','FontSize',15)
+title('Fasmätningar','FontSize',30)
+legend({'Mätning 1','Mätning 2'},'FontSize',15)
+axis([0 repetitions*fftSize/sampRate -4 4])
 
-subplot(1,2,2)
-plot(1:repetitions, unwrap(angle(out2_bin_fft(:,81)) - angle(in_bin_fft(:,81))))
-%%
-plot(1:repetitions, angle(in_bin_fft(:,81)))
+% matlab2tikz dynamic LaTeX-figures
+matlab2tikz('../../signalbehandling/figures/phase-difference.tex')
 
-%%
-k = 0;
-for i=1:length(out1_bin_fft)
-    if max(abs(out1_bin_fft(i,:))) > 0.3
-        k = i
-        break;
-    end
-end
-f = 1:fft_size;
-subplot(1,3,1)
-plot(f, abs(out1_bin_fft(k,:)))
+% Plot example signal
+t = (1:length(reOut1));%/sampRate;
+t = (length(t)/2:length(t)/2+100);
 
-subplot(1,3,2)
-plot(f, abs(out2_bin_fft(k,:)))
+figure('rend','painters','pos',[10 10 700 300])
+plot((t-t(1))*1e3/sampRate, reOut1(t),'black','LineWidth',1);
+%set(gca,'YTickLabel',[],'FontSize',15)
+ylabel('Spänning (godtycklig skala)')
+xlabel('Tid (ms)')
+axis([0 (t(end)-t(1))*1e3/sampRate -4e-4 4e-4])
+matlab2tikz('../../signalbehandling/figures/example-signal.tex')
 
-subplot(1,3,3)
-plot(f, abs(in_bin_fft(k,:)))
-
-
-%%
-t = (1:length(re_out1));%/samp_rate;
-
-subplot(1,3,1)
-plot(t, re_out1);
-
-subplot(1,3,2)
-plot(t, re_out2);
-
-
-subplot(1,3,3)
-plot(t, re_in);
+% Plot the reference signal
+figure('rend','painters','pos',[10 10 700 300])
+plot((t-t(1))*1e3/sampRate, reIn(t),'black','LineWidth',1);
+set(gca,'YTickLabel',[],'FontSize',15)
+ylabel('Spänning (godtycklig skala)')
+axis([0 (t(end)-t(1))*1e3/sampRate -1 1])
+matlab2tikz('../../signalbehandling/figures/reference-signal.tex')
