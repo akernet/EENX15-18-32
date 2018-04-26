61ba2e47cbfdf4cedab2c681822de097c19460aa
Jens Åkerlund <jens@akernet.se>
2018-04-26 15:28:51 +0200

    Fix for parameters

--- signalinsamling/util.hpp
+++ signalinsamling/util.hpp
@@ -2,7 +2,7 @@ static bool stop_signal_called = false;
 
 void init_matrix();
 
-void switch_matrix(int port1, int port2);
+void switch_matrix(int port1, int port2, float delay);
 
 void switch_antenna(uhd::usrp::multi_usrp::sptr usrp_device, float time);
 
